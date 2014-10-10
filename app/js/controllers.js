40cb99de3d386b9a95257a4a872fee6995d8c6c6
Ivan Arrizabalaga Getino <ivan.arrizabalaga@gmail.com>
2014-10-10 15:05:32 +0200

    Chapter 12 done

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -6,6 +6,11 @@ angular.module('myApp.controllers', [])
   .controller('LandingPageController', [function() {
 
   }])
-  .controller('WaitlistController',[function(){
-  	
+  .controller('WaitlistController',['$scope',function($scope){
+  	$scope.parties = [];
+  	$scope.party = {name:'',phone:'',size:''};
+  	$scope.saveParty = function(){
+  		$scope.parties.push($scope.party);
+  		$scope.party = {name:'',phone:'',size:''};
+  	};
   }]);
