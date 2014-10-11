04e309f54224b549df1e6906cb6b298ff97c1935
Ivan Arrizabalaga <ivan.arrizabalaga@gmail.com>
2014-10-11 23:27:24 +0200

    Chapter 17 done

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -7,12 +7,23 @@ angular.module('myApp.controllers', [])
 
   }])
   .controller('WaitlistController',['$scope','$firebase',function($scope,$firebase){
-  	var partiesRef = new Firebase('https://waitandeat-ivan.firebaseio.com/');
-
+  	//Connect $scope.parties to live Firebase data.
+  	var partiesRef = new Firebase('https://waitandeat-ivan.firebaseio.com/parties');
   	$scope.parties = $firebase(partiesRef);
+
+  	//Object to store data from the waitlist form
   	$scope.newParty = {name:'',phone:'',size:''};
+
+  	//Adds a party to firebase
   	$scope.saveParty = function(){
   		$scope.parties.$add($scope.newParty);
   		$scope.newParty = {name:'',phone:'',size:''};
+  	};
+
+  	//Sends a SMS to a party
+  	$scope.sendTextMessage = function(phoneNumber){
+  		var textMessageRef = new Firebase('https://waitandeat-ivan.firebaseio.com/textMessages');
+		var textMessages = $firebase(textMessageRef);
+		textMessages.$add({phoneNumber:phoneNumber});
   	};
   }]);
