132ec0a5981eea17e8c3858736cfc0ee3b6a7f91
Ivan Arrizabalaga Getino <ivan.arrizabalaga@gmail.com>
2014-10-14 15:10:11 +0200

    Chapter 21 done

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -3,27 +3,46 @@
 /* Controllers */
 
 angular.module('myApp.controllers', [])
-  .controller('LandingPageController', [function() {
+    .controller('LandingPageController', [function() {
 
-  }])
-  .controller('WaitlistController',['$scope','$firebase',function($scope,$firebase){
-  	//Connect $scope.parties to live Firebase data.
-  	var partiesRef = new Firebase('https://waitandeat-ivan.firebaseio.com/parties');
-  	$scope.parties = $firebase(partiesRef);
+    }])
+    .controller('WaitlistController', ['$scope', '$firebase', function($scope, $firebase) {
+        //Connect $scope.parties to live Firebase data.
+        var partiesRef = new Firebase('https://waitandeat-ivan.firebaseio.com/parties');
+        $scope.parties = $firebase(partiesRef);
 
-  	//Object to store data from the waitlist form
-  	$scope.newParty = {name:'',phone:'',size:''};
+        //Object to store data from the waitlist form
+        $scope.newParty = {
+            name: '',
+            phone: '',
+            size: '',
+            done: false,
+            notified:'No'
+        };
 
-  	//Adds a party to firebase
-  	$scope.saveParty = function(){
-  		$scope.parties.$add($scope.newParty);
-  		$scope.newParty = {name:'',phone:'',size:''};
-  	};
+        //Adds a party to firebase
+        $scope.saveParty = function() {
+            $scope.parties.$add($scope.newParty);
+            $scope.newParty = {
+                name: '',
+                phone: '',
+                size: '',
+                done: false,
+                notified:'No'
+            };
+        };
 
-  	//Sends a SMS to a party
-  	$scope.sendTextMessage = function(phoneNumber){
-  		var textMessageRef = new Firebase('https://waitandeat-ivan.firebaseio.com/textMessages');
-		var textMessages = $firebase(textMessageRef);
-		textMessages.$add({phoneNumber:phoneNumber});
-  	};
-  }]);
+        //Sends a SMS to a party
+        $scope.sendTextMessage = function(party) {
+            var textMessageRef = new Firebase('https://waitandeat-ivan.firebaseio.com/textMessages');
+            var textMessages = $firebase(textMessageRef);
+            var newTextMessage = {
+                phoneNumber: party.phone,
+                size: party.size,
+                name: party.name
+            };
+            textMessages.$add(newTextMessage);
+            party.notified='Yes';
+            $scope.parties.$save(party.$id);
+        };
+    }]);
